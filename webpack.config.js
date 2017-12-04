f9de26ca2947c168e0bf4a3f9a0599b559f406d8
Logan Cooper <ldtcoop@gmail.com>
2017-12-04 14:06:52 -0800

    Add html webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,3 +11,24 @@ module.exports = {
      ]
  }
 };
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+};
