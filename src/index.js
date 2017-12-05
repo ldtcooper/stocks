2d7592686aa1888144f5739d5694d0b3d5f1cca2
Logan Cooper <ldtcoop@gmail.com>
2017-12-05 10:23:05 -0800

    Fix typo

--- src/index.js
+++ src/index.js
@@ -9,31 +9,24 @@ const yearParser = d3.timeParse("%Y");
 const x = d3.scaleTime().range([0, width]);
 const y = d3.scaleLinear().range(height, 0);
 
-const lowIncomeLine = d3.line()
-                    .x( (d) => x(d["Year"]))
-                    .y( (d) => y(d["Low Income"]));
-
-const lowMiddleLine = d3.line()
-                    .x( (d) => x(d["Year"]))
-                    .y( (d) => y(d["Lower Middle Income"]));
-
-const upperMiddleLine = d3.line()
-                    .x( (d) => x(d["Year"]))
-                    .y( (d) => y(d["Upper Middle Income"]));
-
-const highIncomeLine = d3.line()
-                    .x( (d) => x(d["Year"]))
-                    .y( (d) => y(d["High Income"]));
+// const lowIncomeLine = d3.line()
+//                     .x( (d) => x(d["Year"]))
+//                     .y( (d) => y(d["Low Income"]));
+//
+// const lowMiddleLine = d3.line()
+//                     .x( (d) => x(d["Year"]))
+//                     .y( (d) => y(d["Lower Middle Income"]));
+//
+// const upperMiddleLine = d3.line()
+//                     .x( (d) => x(d["Year"]))
+//                     .y( (d) => y(d["Upper Middle Income"]));
+//
+// const highIncomeLine = d3.line()
+//                     .x( (d) => x(d["Year"]))
+//                     .y( (d) => y(d["High Income"]));
 
 viz.append("g").attr("transform", `translate(0,${height})`);
 
-d3.csv('../data/women_in_gov_clean.csv', (data) => {
-  data.forEach( (d) => {
-    d["Year"] = yearParser(d["Year"]);
-  });
-
-  x.domain(d3.extent(data, (d) => d["Year"]));
-  y.domain([0, 50]);
-
-  viz.append("g").call(d3.axisBottom(x));
+d3.json('../data/women_in_gov_clean.json', (data) => {
+  console.log(data);
 });
