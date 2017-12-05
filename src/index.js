641b0b29cf2555fab39fa6a6f6b8776122513b93
Logan Cooper <ldtcoop@gmail.com>
2017-12-04 22:31:26 -0800

    Lay groundwork for line chart

--- src/index.js
+++ src/index.js
@@ -9,9 +9,24 @@ const parser = d3.timeParse("%Y");
 const x = d3.scaleTime().range([0, width]);
 const y = d3.scaleLinear().range(height, 0);
 
-// const valueLine = d3.line()
-//                     .x( (d) => x(d.))
-//                     .y( (d) => y(d.))
-d3.csv('../data/women_in_gov.csv', (data) => {
+const lowIncomeLine = d3.line()
+                    .x( (d) => x(d["Year"]))
+                    .y( (d) => y(d["Low Income"]));
+
+const lowMiddleLine = d3.line()
+                    .x( (d) => x(d["Year"]))
+                    .y( (d) => y(d["Lower Middle Income"]));
+
+const upperMiddleLine = d3.line()
+                    .x( (d) => x(d["Year"]))
+                    .y( (d) => y(d["Upper Middle Income"]));
+
+const highIncomeLine = d3.line()
+                    .x( (d) => x(d["Year"]))
+                    .y( (d) => y(d["High Income"]));
+
+viz.append("g").attr("transform", `translate(0,${height})`);
+
+d3.csv('../data/women_in_gov_clean.csv', (data) => {
   console.log(data);
 });
