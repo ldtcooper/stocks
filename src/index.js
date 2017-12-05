df2348f19b23cc7fb39f0bd8eb2924709aeac55b
Logan Cooper <ldtcoop@gmail.com>
2017-12-05 14:51:26 -0800

    Build axes

--- src/index.js
+++ src/index.js
@@ -7,20 +7,40 @@ const width = 1100;
 const yearParser = d3.timeParse("%Y");
 
 const x = d3.scaleTime().range([0, width]);
-const y = d3.scaleLinear().range(height, 0);
+const y = d3.scaleLinear().range([height, 0]);
 
-viz.append("g").attr("transform", `translate(0,${height})`);
+const g = viz.append("g").attr("transform", `translate(0,${height})`);
 
 d3.csv('../data/women_in_gov_clean.csv', (data) => {
   const tranches = data.columns.slice(1).map( (id) => {
     return {
       id: id,
       values: data.map( (d) => {
-        return {year: d["Year"], share: parseFloat(d[id])};
+        return {year: d["Year"], share: d[id] ? parseFloat(d[id]) : null};
       })
     };
   });
 
+  console.log(tranches);
+
   x.domain(d3.extent(data, (d) => d.date));
   y.domain([0, 50]);
+
+  g.append("g")
+   .attr("class", "axis axis--x")
+   .attr("transform", `translate(0,${height})`)
+   .call(d3.axisBottom(x));
+
+  g.append("g")
+   .attr("class", "axis axis--y")
+   .call(d3.axisLeft(y))
+   .append("text")
+   .attr("transform", "rotate(-90)")
+   .attr("y", 6)
+   .attr("dy", "0.75em")
+   .attr("fill", "black")
+   .text("Proportion of Women in National Parliamant");
+
+  const tranche = g.selectAll()
+
 });
